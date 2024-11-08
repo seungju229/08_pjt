85962b54ccad2699fcfddc5535193a1980562d85
seungju425 <seungju425@gmail.com>
2024-11-08 17:54:25 +0900

    filter_genre 다시 작성하기

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_safe
-
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_safe, require_http_methods
+from .serializers import GenreSerializers
+from .models import Genre, Movie
 
 # Create your views here.
 @require_safe
 def index(request):
-    pass
+    genres = Genre.objects.all()
+    movies = Movie.objects.all()
+    context = {
+        'genres':genres,
+        'movies':movies
+    }
+    return render(request, 'movies/index.html', context)
 
-
+@require_safe
 def filter_genre(request):
-    pass
+    movies = Movie.objects.get()
+
 
 
 @require_safe
